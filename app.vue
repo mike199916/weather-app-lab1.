ebbfd1ac3a1ffebe4c964855cf6b984f59a51e07
mike199916 <33725192+mike199916@users.noreply.github.com>
2020-03-30 11:39:38 +0300

    Create app.vue

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'app'
+    data () {
+        return
+        api_key: '7ea4ff53192332000e1b6463db601ac3'
+    }
+}
+ </script>
+
+ <style>
+
+ </style>
